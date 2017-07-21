6680b519924f7fb3e62d857c62bc3f8a22f5bbaf
viorel.tacliciu <viorel.tacliciu@teamnet.ro>
2017-07-21 03:21:13 +0300

    doNotCommit

--- src/main/java/ro/teamnet/zth/api/database/DBProperties.java
+++ src/main/java/ro/teamnet/zth/api/database/DBProperties.java
@@ -1,17 +1,16 @@
 package ro.teamnet.zth.api.database;
 
-/**
- * Created by Raluca.Russindilar on 02.07.2016.
- */
+
 public interface DBProperties {
     //TODO de inlocuit cu IP-ul din Docker
-    String IP = "192.168.99.100";
+    String IP = "localhost";
     String PORT = "49161";
     //TODO de inlocuit 1cu utilizatorul vostru
     String USER = "system";
     //TODO de inlocuit cu parola voastra
     String PASS = "oracle";
     String SID = "xe";
+    String SCHEMA = "HR";
     String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
 
 }
